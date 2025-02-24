a545a780204ab1cb9285780c562ad2907d364421
Gustavo Berlanga <74972093+Berla1@users.noreply.github.com>
2025-02-24 12:20:36 -0300

    adicionando spring security e modelando a classe user

---
+++ src/main/java/med/voll/med_voll/model/Usuario.java
@@ -0,0 +1,40 @@
+package med.voll.med_voll.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Usuario setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public Usuario setLogin(String login) {
+        this.login = login;
+        return this;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public Usuario setSenha(String senha) {
+        this.senha = senha;
+        return this;
+    }
+}
